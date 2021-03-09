6dc72a4a80700ce34392daf459baa78f3c33f1d7
hghghgl <mmoragamolina@gmail.com>
2021-03-09 20:12:48 +0100

    save

--- metropolix/src/Controller/RegisterController.php
+++ metropolix/src/Controller/RegisterController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RegisterController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function index(): Response
+    public function register(Request $request, EntityManagerInterface $em): Response
     {
+        $username = $request->request->get('username');
+
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
         ]);
